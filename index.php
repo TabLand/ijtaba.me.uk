2b14db1e460d5c924a6dce8fb0bfb0dc3a929502
Ijtaba Hussain <ijtabahussain@live.com>
2015-07-31 21:25:56 +0100

    Bit more professional

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
 	<body>
 		Hello, and welcome to <?php echo whats_my_name();?> Hussain's personal webpages.<br>
 		Here is my Realtime <a href="calendar/">Schedule</a><br>
-		From here you can <a href="dine_me.html">Dine Me </a><br>
+		From here you can <a href="dine_me/">Dine Me </a><br>
 		Or <a href="rob">Rob Me</a><br>
 		Or <a href="last_sighting/">Kidnap Me</a><br>
-		Or <a href="hire_me/">Enslave Me</a><br>
+		Or <a href="hire_me/">Hire Me</a><br>
 		Or link me on <a href="linkedin/">LinkedIn</a><br>
 		I can be reached on the email <a href="email"><?php echo whats_my_name();?>hussain@live.com</a><br>
 		You can <a href="call">call</a> or <a href="sms">text</a> me on my mobile number <a href="call_me_maybe">(0044) 07876363428</a><br>
