35dc56affee96dd7798982709ff1f2ef1c24197f
Ijtaba Hussain <ijtabahussain@live.com>
2015-07-31 22:56:04 +0100

    Validate referers even further. Fixes #1

--- name.php
+++ name.php
@@ -1,20 +1,32 @@
 <?php
     function whats_my_name() {
-        if(isset($_GET['name'])) $my_name = $_GET['name']; 
-        elseif(isset($_SERVER['HTTP_REFERER'])) $my_name = $_SERVER['HTTP_REFERER'];
-        else $my_name = "Ijtaba";
+        if(isset($_SERVER['HTTP_REFERER'])){ 
+            $my_name = check_list($_SERVER['HTTP_REFERER']);
+        }
+        else {
+            $my_name = "Ijtaba";
+        }
+        return $my_name;
+    }
 
-       $my_name = preg_replace("/https?:\/\//", "",$my_name);
-       $my_name = preg_replace("/\.tk/", "",$my_name);
-       $matches = array();
-       $my_name = preg_match("/[A-Za-z]+/",$my_name,$matches);
-
-       if(count($matches)==1) {
-            $my_name = ucfirst($matches[0]);
-            return $my_name;
-       }
-       else{
-            return "Ijtaba";
-       }
+    function check_list($referer){
+         $names   = file('names.list',FILE_IGNORE_NEW_LINES);
+         $my_name = preg_replace("/https?:\/\//", "",$referer);
+         $my_name = preg_replace("/\.tk/",        "",$my_name);
+         $my_name = preg_replace("/www\./",       "",$my_name);
+         $matches = array();
+         $my_name = preg_match("/[A-Za-z]+/",$my_name,$matches);
+         
+         if(count($matches)==1){
+            if(in_array($matches[0],$names)){
+                return ucfirst($matches[0]);
+            }
+            else {
+                return "Ijtaba";
+            }
+         }
+         else {
+             return "Ijtaba";
+         }
     }
 ?>
