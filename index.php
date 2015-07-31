83bf1a6d127d03312117e2ca3fd270c447fe6185
Ijtaba Hussain <ijtabahussain@live.com>
2015-07-31 22:56:30 +0100

    Lowercase email addresses look neater

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		Or <a href="last_sighting/">Locate Me</a><br>
 		Or <a href="hire_me/">Hire Me</a><br>
 		Or link me on <a href="linkedin/">LinkedIn</a><br>
-		I can be reached on the email <a href="email"><?php echo whats_my_name();?>hussain@live.com</a><br>
+		I can be reached on the email <a href="email"><?php echo lcfirst(whats_my_name());?>hussain@live.com</a><br>
 		You can <a href="call">call</a> or <a href="sms">text</a> me on my mobile number <a href="call_me_maybe">(0044) 07876363428</a><br>
 	</body>
 </html>
