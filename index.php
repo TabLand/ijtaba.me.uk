f2626d6c70eb0d7baab2d29e893418f83f75be7d
Ijtaba Hussain <ijtabahussain@live.com>
2015-08-06 17:04:44 +0100

    Remove last sighting till app actually exists, add margin

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 		<link rel="stylesheet" type="text/css" href="main.css">
 	</head>
 	<body>
+        <br/><br/>
 		Hello, and welcome to <?php echo whats_my_name();?> Hussain's personal webpages.<br>
 		Here is my Realtime <a href="calendar/">Schedule</a><br>
 		From here you can <a href="dine_me/">Dine Me </a><br>
-		Or <a href="last_sighting/">Locate Me</a><br>
 		Or <a href="hire_me/">Hire Me</a><br>
 		Or link me on <a href="linkedin/">LinkedIn</a><br>
 		I can be reached on the email <a href="email"><?php echo lcfirst(whats_my_name());?>hussain@live.com</a><br>
