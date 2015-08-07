4c0bd72aff002650ba4ae9a345925c801d612162
Ijtaba Hussain <ijtabahussain@live.com>
2015-08-07 09:42:29 +0100

    Better telephony links

---
+++ call/index.php
@@ -0,0 +1,12 @@
+<?php require "../name.php"; ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+	<head>
+		<title>Call <?php echo whats_my_name();?> Hussain</title>
+		<link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="../main.css">
+	</head>
+	<body>
+        <a class="telephony" href="tel:00447876363428">(&#128222;)</a>
+	</body>
+</html>
