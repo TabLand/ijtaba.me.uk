550f1eb10d3d490f4cae2bc10c9fb45c0e882ab4
Ijtaba Hussain <ijtabahussain@live.com>
2015-08-07 09:36:23 +0100

    Conform to W3C Standards, Should add an emailable check to show if I passed validation

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php require "name.php"; ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
+        <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 		<title><?php echo whats_my_name();?> Hussain's Personal Webpages</title>
 		<link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="main.css">
 	</head>
 	<body>
-        <br/><br/>
 		Hello, and welcome to <?php echo whats_my_name();?> Hussain's personal webpages.<br>
 		Here is my Realtime <a href="calendar/">Schedule</a><br>
 		From here you can <a href="dine_me/">Dine Me </a><br>
