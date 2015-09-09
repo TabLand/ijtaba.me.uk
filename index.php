48e21a88ad2eaeeaeedf7663788cb5c60e96df8c
Ijtaba Hussain <ijtabahussain@live.com>
2015-09-09 23:28:42 +0100

    Car rental

--- index.php
+++ index.php
@@ -6,13 +6,17 @@
 		<title><?php echo whats_my_name();?> Hussain's Personal Webpages</title>
 		<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-		Hello, and welcome to <?php echo whats_my_name();?> Hussain's personal webpages.<br>
-		Here is my Realtime <a href="calendar/">Schedule</a><br>
-		From here you can <a href="dine_me/">Dine Me </a><br>
-		Or link me on <a href="linkedin/">LinkedIn</a><br>
-		I can be reached on the email <a href="email"><?php echo lcfirst(whats_my_name());?>hussain@live.com</a><br>
-		You can <a href="call">call</a> or <a href="sms">text</a> me on my mobile number <a href="call_me_maybe">(0044) 07876363428</a><br>
+        <div id="index_container">
+            Hello, and welcome to <?php echo whats_my_name();?> Hussain's personal webpages.<br>
+            Here is my Realtime <a href="calendar/">Schedule</a><br>
+            From here you can <a href="dine_me/">Dine Me </a><br>
+            Or link me on <a href="linkedin/">LinkedIn</a><br>
+            Or <a href="rent-my-car/">borrow</a> my car<br>
+            I can be reached on the email <a href="email"><?php echo lcfirst(whats_my_name());?>hussain@live.com</a><br>
+            You can <a href="call">call</a> or <a href="sms">text</a> me on my mobile number <a href="call_me_maybe">(0044) 07876363428</a><br>
+        </div>
 	</body>
 </html>
