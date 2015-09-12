acb6eda3ea16fcb5e3082046539a8360d24ea79e
Ijtaba Hussain <ijtabahussain@live.com>
2015-09-12 22:52:21 +0100

    Rent my car improvements

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             Here is my Realtime <a href="calendar/">Schedule</a><br>
             From here you can <a href="dine_me/">Dine Me </a><br>
             Or link me on <a href="linkedin/">LinkedIn</a><br>
-            Or <a href="rent-my-car/">borrow</a> my car<br>
+            Or <a href="rent-my-car">borrow</a> my car<br>
             I can be reached on the email <a href="email"><?php echo lcfirst(whats_my_name());?>hussain@live.com</a><br>
             You can <a href="call">call</a> or <a href="sms">text</a> me on my mobile number <a href="call_me_maybe">(0044) 07876363428</a><br>
         </div>
