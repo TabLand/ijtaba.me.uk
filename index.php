087bc9487e6ea7f6e8bf6a8621df3beb02dd74d7
Ijtaba Hussain <ijtabahussain@live.com>
2015-11-18 05:17:36 +0000

    Added responsiveness

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
             Follow me on <a href="twitter">twitter</a><br>
             I can be reached on the email <a href="email">ijtabahussain@live.com</a><br>
             You can <a href="call">call</a> or <a href="sms">text</a> me on <a href="call_me_maybe">00447876363428</a><br>
+            <?php require "rent-my-car/responsiveness" ?><br/>
             You can fax me on <a href="fax:00448721116047">00448721116047</a>, subject to <a href="fax_charges/">charges</a>
         </div>
 	</body>
