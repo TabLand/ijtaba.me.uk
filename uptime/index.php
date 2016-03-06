6358514e406fc7e7bde1dc6620f203ed72abe16c
Ijtaba Hussain <ijtabahussain@live.com>
2016-03-06 19:25:17 +0000

    Pingdom no longer offering uptime graphs for free

--- uptime/index.php
+++ uptime/index.php
@@ -23,16 +23,6 @@
         <div id="index_container">
             Internal uptime (OS) is : <?php show_uptime("os")?><br/>
             Internal uptime (Web Server) is : <?php show_uptime("apache2")?><br/>
-               
-            Pingdom stats for the last month given below:<br/>
-            <a href="http://www.pingdom.com">
-                Uptime Report<br/>
-                <img src="https://share.pingdom.com/banners/211f2b6e" alt="Uptime Report for ijtaba.me.uk: Last 30 days" title="Uptime Report for ijtaba.me.uk: Last 30 days" width="300" height="165" />
-            </a><br/>
-            <a href="http://www.pingdom.com">
-                Response Time Report<br/>
-                <img src="https://share.pingdom.com/banners/ead484da" alt="Response Time Report for ijtaba.me.uk: Last 30 days" title="Response Time Report for ijtaba.me.uk: Last 30 days" width="300" height="165" />
-            </a>
         </div>
 	</body>
 </html>
