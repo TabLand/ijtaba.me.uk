23652210e65b4aff79579f2b384d1481c2c77a27
Ijtaba Hussain <ijtabahussain@live.com>
2016-11-13 21:01:09 +0000

    Correct http status codes

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php 
+    http_response_code(404);
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+	<head>
+		<title>Since you been gone!</title>
+		<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		Hey there beautiful,<br/>
+		Is it <a href="https://ijtaba.me.uk">me</a> you're looking for?
+	</body>
+</html>
